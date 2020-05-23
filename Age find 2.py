4a634c73e41208f0e5ea04bc8623668ad2d409d5
python Beginner Project <64841811+sudharson31@users.noreply.github.com>
2020-05-23 06:35:18 -0400

    Add files via upload

---
+++ Age find 2.py
@@ -0,0 +1,21 @@
+print("     -----------     ")
+print("     |               ")
+print("     |               ")
+print("     -----------     ")
+print("                |    ")
+print("                |    ")
+print("     -----------     ")
+
+
+name = input("Enter your name: ")
+year = int(input("Enter your D.O.B year: "))
+
+age = str(2020-year)
+print(name + " your age is: " + age)
+
+if age >age:
+    print("You are eligible for voting:")
+else:
+    print("You are not eligible for vote")
+
+print("---------------Thanks For Using---------------")
